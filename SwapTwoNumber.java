8ad9ceb88dd2b2c9958e0ad6c5a3126d34a7897b
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-07-06 01:07:59 +0530

    Program to swap two numbers

---
+++ SwapTwoNumber.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+		int num1 = 45, num2=78;
+		int temp;
+		System.out.println("Number before swap are ="+num1+" "+num2);
+		temp=num1;
+		num1=num2;
+		num2=temp;
+		System.out.println("Number after swap are ="+num1+" "+num2);
+
+	}
+
+}
